b103110c673a95ba98fa14b5702a0210bc787bbc
Enzo <enzo.biamonti@epitech.eu>
2021-11-15 02:33:20 +0100

    fix config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    "api_url": process.env.NODE_ENV !== 'production' ?
+    "api_url": process.env.NODE_ENV === 'production' ?
         "https://node-backend-pwa.herokuapp.com" : " http://localhost:4000",
     "vapidPublicKey": "BM0AYWnrNIo1NGaYDYgok4I-xtyB1NZidBV0MtUKEdx8jKIDaO7g8b9eOjNAOgFuf80mSyyGGoFUf3UNOBK_lqQ"
 
